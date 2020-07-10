47f55c57a98c1096e25c488555535d43477a623a
shareed <shanreed6@gmail.com>
2020-07-09 17:16:34 -0700

    added basic CRUD for assigners

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Duty-Server
-* Server Created Using ReadMe From [Nodejs-Express-Server Repo](https://github.com/shareed/Nodejs-Express-Server/tree/express-server)
+* Server Created Using ReadMe From [Nodejs-Express-Server Repo](https://github.com/shareed/Nodejs-Express-Server/tree/adding-routes)
+
+## RESTful Web API
+* Manages Assigners and Assignees
+**Assigner** *users that assign task to other users*
+    * unique id
+    * username
+    * email
+    * phone number
+    * password
+
+
+* Assigness - users that get assign tasks
+    * unique id
+    * username
+    * email
+    * phone number
+    * password
+    * assigner id that connects to a assigner(can have more than one).
+
+### Assigner
+- [x] View a list of existing Assigners.
+- [x] View the details of a single Assigner
+- [x] Update the information of an existing Assigner
+- [x] Remove a Assigner.
+
+### Assignee
+- [ ] Add a Assignee.
+- [ ] View a list of existing Assignees.
+- [ ] View the details of a single Assignee
+- [ ] Update the information of an existing Assignee.
+- [ ] Remove a Assignee.
+
+
+
+Here is a table with the `endpoint` descriptions:
+
+| Action               | URL               | Method | Response         |
+| :------------------- | :---------------- | :----- | :--------------- |
+| Add a Assigner         | /api/assigners      | POST   | the new Assigner   |
+| View list of Assigners | /api/assigners      | GET    | array of Assigners |
+| View Assigner details  | /api/assigners/{id} | GET    | a Assigner         |
+| Update Assigner        | /api/assigners/{id} | PATCH  | updated Assigner   |
+| Remove a Assigner      | /api/assigners/{id} | DELETE | deleted Assigner   |
+| Add a Assignee            | /api/assignees         | POST   | the new Assignee      |
+| View list of Assignees    | /api/assignees         | GET    | array of Assignees    |
+| View Assignee details     | /api/assignees/{id}    | GET    | a Assignee            |
+| Update Assignee           | /api/assignees/{id}    | PATCH  | updated Assignee      |
+| Remove a Assignee         | /api/assignees/{id}    | DELETE | deleted Assignee      |
--- index.js
+++ index.js
@@ -1,13 +1,95 @@
 require('dotenv').config()
 const server = require('./api/server.js');
+const shortid = require('shortid')
 
 
-server.get('/', (req, res) => {
-    const message = process.env.EVNM;
-    res.send(message)
+let assigners = []
+
+//CRUD
+
+
+
+
+//READ
+server.get('/api/assigners', (req, res) => {
+    res.json(assigners)
 })
 
-const port = process.env.PORT;
+server.get('/api/assigners/:id', (req, res) => {
+    const { id } = req.params; 
+
+    const found = assigners.find(assigner => assigner.id === id);
+    if (found) {
+        res.status(200).json(found)
+    } else {
+        res.status(404).json({meassage: "assigner not found"});
+    }
+})
+
+//CREATE
+server.post('/api/assigners', (req, res) => {
+    const assignerInfo = req.body; 
+    assignerInfo.id = shortid.generate(); 
+    assigners.push(assignerInfo) 
+    res.status(201).json(assignerInfo)
+})
+
+
+//UPDATE
+
+
+//DELETE
+server.delete('/api/assigners/:id', (req, res) => {
+    const { id } = req.params; 
+
+    const deleted = assigners.find(assigner => assigner.id === id);
+    if (deleted) {
+        assigners= assigners.filter(assigner => assigner.id !== id);
+        res.status(200).json({Deleted:deleted })
+    } else {
+        res.status(404).json({meassage: "assigner not found"});
+    }
+})
+
+
+//UPDATE- Change
+server.patch('/api/assigners/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    let found = assigners.find(assigner => assigner.id === id);
+
+    if (found) {
+        Object.assign(found, changes);
+        res.status(200).json(found);
+
+    } else {
+        res.status(404).json({message: "assigner not found"})
+    }
+})
+
+
+
+//UPDATE Rplace
+server.put('/api/assigners/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    let index = assigners.findIndex(assigner => assigner.id === id); 
+    
+    if (index !== -1) {
+        assigners[index] = changes
+        res.status(200).json(assigners[index]);
+
+    } else {
+        res.status(404).json({message: "assigner not found"})
+    }
+})
+
+
+
+
+const port =process.env.PORT; 
 
 
 server.listen(port, () => console.log(`\n*** Server Running on http://localhost:${port} ***\n`))
